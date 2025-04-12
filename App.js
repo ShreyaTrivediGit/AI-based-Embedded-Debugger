025e8f3f2a93817734c1d4881d38ec9cf4617f0d
ShreyaTrivediGit <89206210+ShreyaTrivediGit@users.noreply.github.com>
2025-04-12 10:22:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ProteusDebugger from './components/ProteusDebugger';
+
+function App() {
+  return (
+    <div className="App">
+      <ProteusDebugger />
+    </div>
+  );
+}
+
+export default App;
